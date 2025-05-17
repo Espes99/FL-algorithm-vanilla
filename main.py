5c9deff534cc63565e881db4e5e2de3d0022aa4d
Espen Sele <espen.selle@gmail.com>
2025-05-17 21:20:04 +0200

    remove unused import

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
 import tensorflow as tf
